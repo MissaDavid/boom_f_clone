59136c0d26fdedafbcd84de2218fae76e542c9f4
Mel Tamplin <mei.chohee@gmail.com>
2022-09-14 11:33:07 -0400

    feat(assets): csv layouts for level one

---
+++ assets/tilesets/fixed.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="fixed" tilewidth="32" tileheight="32" tilecount="8" columns="8">
+ <image source="../fixed.png" width="256" height="32"/>
+</tileset>
