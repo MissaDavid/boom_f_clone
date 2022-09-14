880328bd948bbc97b665c214e7f7ac37333a821a
Mel Tamplin <mei.chohee@gmail.com>
2022-09-14 18:21:48 -0400

    feat(tilesets): added player one spritesheet
    Signed-off-by: Mel Tamplin <mei.chohee@gmail.com>

--- assets/tilesets/border1.tsx
+++ assets/tilesets/border1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.1" name="border1" tilewidth="32" tileheight="32" tilecount="255" columns="17">
- <image source="../border1.png" width="544" height="480"/>
+ <image source="border1.png" width="544" height="480"/>
 </tileset>
---
+++ assets/tilesets/player_one.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="player_one" tilewidth="32" tileheight="32" tilecount="38" columns="19">
+ <image source="player_one.png" width="608" height="64"/>
+</tileset>
