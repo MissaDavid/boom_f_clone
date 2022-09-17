1f9846524e5320f2614d13068992d7df59ca6775
Mel Tamplin <mei.chohee@gmail.com>
2022-09-17 13:00:35 -0400

    feat(player): Boom can move in every direction (no collision)
    Signed-off-by: Mel Tamplin <mei.chohee@gmail.com>

--- player.py
+++ player.py
@@ -13,8 +13,9 @@ class Player(pygame.sprite.Sprite):
         self.image = self.animations["idle"]
         self.rect = self.image.get_rect(topleft=position)
 
-        # Vector2 is a list that contains (x, y) values
-        self.direction = pygame.math.Vector2(position)
+        self.direction = pygame.math.Vector2(0, 0)
+        self.facing_right = False
+        self.facing_down = False
         self.speed = 4
 
     def set_animation_sprites(
@@ -31,20 +32,45 @@ class Player(pygame.sprite.Sprite):
         }
 
     def set_direction(self):
-        keys = pygame.key.get_pressed()
+        """
+        The Player's moving direction is a Vector2 representing x, y axis
+        0, 0 represents the top left corner SO y axis is a positive in when going DOWN
 
-        if keys[pygame.K_LEFT]:
-            self.direction.x = -1
-        elif keys[pygame.K_RIGHT]:
-            self.direction.x = 1
-        elif keys[pygame.KEYUP]:
-            self.direction.y = 1
-        elif keys[pygame.KEYDOWN]:
-            self.direction.y = -1
+        x:
+            Going right == 1, going left == -1
+        y:
+            Going down == 1, going up == -1
+        """
+        key_state = pygame.key.get_pressed()
+
+        self.direction.x = key_state[pygame.K_RIGHT] - key_state[pygame.K_LEFT]
+        self.direction.y = key_state[pygame.K_DOWN] - key_state[pygame.K_UP]
+
+        self.facing_right = True if self.direction.x == 1 else False
+        self.facing_down = True if self.direction.y == 1 else False
 
     def move(self):
-        pass
+        x = self.direction.x
+        y = self.direction.y
+
+        if x > 0:
+            self.image = self.animations["walk_right"][0]
+            self.rect.move_ip(x * self.speed, y)
+        elif x < 0:
+            self.image = self.animations["walk_left"][0]
+            self.rect.move_ip(x * self.speed, y)
+        elif y > 0:
+            self.image = self.animations["walk_down"][0]
+            self.rect.move_ip(x, y * self.speed)
+        elif y < 0:
+            self.image = self.animations["walk_up"][0]
+            self.rect.move_ip(x, y * self.speed)
+        else:
+            self.image = self.animations["idle"]
 
     def update(self):
+        """
+        Get key input to set the direction, then move
+        """
         self.set_direction()
         self.move()
