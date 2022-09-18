ce882987188ebdd5e5badf00d06d80ef0f28ddf6
Mel Tamplin <mei.chohee@gmail.com>
2022-09-18 12:18:19 -0400

    style(utils): renaming variables and adding type for better clarity
    Signed-off-by: Mel Tamplin <mei.chohee@gmail.com>

--- utils.py
+++ utils.py
@@ -15,23 +15,23 @@ def import_csv_layout(file) -> list:
     return tileset_map
 
 
-def import_sprites(file) -> list:
+def import_sprites(file) -> list[pygame.Surface]:
     sprite_list = []
-    spritesheet = pygame.image.load(file).convert_alpha()
-    x_num_tiles = int(spritesheet.get_size()[0] / TILE_SIZE)
-    y_num_tiles = int(spritesheet.get_size()[1] / TILE_SIZE)
+    spritesheet_surface = pygame.image.load(file).convert_alpha()
+    x_num_tiles = int(spritesheet_surface.get_size()[0] / TILE_SIZE)
+    y_num_tiles = int(spritesheet_surface.get_size()[1] / TILE_SIZE)
 
     for row in range(y_num_tiles):
         for col in range(x_num_tiles):
             x = col * TILE_SIZE
             y = row * TILE_SIZE
 
-            # create a new Surface and blit the sprite on it
-            sprite_rect = pygame.Rect(x, y, TILE_SIZE, TILE_SIZE)
+            # create a new Surface to blit the sprite on it
             # with the SRCALPHA flag, the pixel format will include a per-pixel alpha
             # I use this so the target Surface becomes transparent
+            sprite_rect = pygame.Rect(x, y, TILE_SIZE, TILE_SIZE)
             new_surface = pygame.Surface((TILE_SIZE, TILE_SIZE), pygame.SRCALPHA)
-            new_surface.blit(spritesheet, (0, 0), sprite_rect)
+            new_surface.blit(spritesheet_surface, (0, 0), sprite_rect)
 
             sprite_list.append(new_surface)
 
