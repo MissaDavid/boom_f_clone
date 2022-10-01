67eeda5b48501141d4f1e3e05b49c7cbc9b5304e
Mel Tamplin <mei.chohee@gmail.com>
2022-10-01 13:25:36 -0400

    feat(Level): check for bomb collision with breakables and launch animation
    Signed-off-by: Mel Tamplin <mei.chohee@gmail.com>

--- level.py
+++ level.py
@@ -1,8 +1,9 @@
 import pygame
 
+from bomb import Dispersion, Bomb
 from game_settings import TILE_SIZE, ASSET_FOLDER
-from player import Player, Bomb
-from tile import Tile
+from player import Player
+from tile import Interactive, Tile
 from utils import import_csv_layout, import_sprites
 
 
@@ -32,12 +33,13 @@ class Level:
         self.breakables = self.create_tile_group(
             import_csv_layout(level_data.get("breakables")), "breakable"
         )
+        self.bomb_group = pygame.sprite.Group()
+        self.bomb_dispersion_group = pygame.sprite.Group()
 
         # Player
         self.player_one = self.create_tile_group(
             import_csv_layout(level_data.get("player_one")), "player_one"
         )
-        self.bomb_group = pygame.sprite.Group()
 
     @staticmethod
     def create_tile_group(layout: list, asset_type: str) -> pygame.sprite.Group:
@@ -60,44 +62,74 @@ class Level:
                     x = col_index * TILE_SIZE
                     y = row_index * TILE_SIZE
 
-                    if asset_type == "player_one":
-                        tile = Player((x, y))
-                    else:
-                        sprites = import_sprites(
-                            f"{ASSET_FOLDER}/tilesets/{asset_type}.png"
-                        )
-                        current_sprite = sprites[int(val)]
-                        tile = Tile((x, y), current_sprite)
+                    sprites = import_sprites(
+                        f"{ASSET_FOLDER}/tilesets/{asset_type}.png"
+                    )
+                    current_sprite = sprites[int(val)]
 
-                    sprite_group.add(tile)
+                    if asset_type == "player_one":
+                        sprite_group.add(Player((x, y)))
+                    elif asset_type == "breakable":
+                        sprite_group.add(Interactive((x, y), current_sprite))
+                    else:
+                        sprite_group.add(Tile((x, y), current_sprite))
 
         return sprite_group
+
+    def set_bomb_dispersion(self, bomb) -> None:
+        up = Dispersion((bomb.rect.x, bomb.rect.y - TILE_SIZE))
+        down = Dispersion((bomb.rect.x, bomb.rect.y + TILE_SIZE))
+        left = Dispersion((bomb.rect.x - TILE_SIZE, bomb.rect.y))
+        right = Dispersion((bomb.rect.x + TILE_SIZE, bomb.rect.y))
+        self.bomb_dispersion_group.add([up, down, left, right])
+
+    def can_set_bomb(self, x, y) -> bool:
+        rect = pygame.Rect((x, y), (TILE_SIZE, TILE_SIZE))
+        for sprite in self.bomb_group.sprites():
+            if rect.colliderect(sprite):
+                return False
+
+        return True
+
+    def explode_breakables(self):
+        collisions = []
+        for hitbox in self.bomb_dispersion_group.sprites():
+            breakable = pygame.sprite.spritecollideany(hitbox, self.breakables)
+            if breakable:
+                collisions.append(breakable)
+
+        for block in collisions:
+            print(len(collisions))
+            block.update()
 
     def run(self):
         """
         Update and draw sprites
         """
-        # static tiles
+        # static tiles drawing
         self.border.draw(self.display_surface)
         self.background.draw(self.display_surface)
         self.fixed.draw(self.display_surface)
 
-        # interactive tiles
+        # interactive tiles drawing
         self.breakables.draw(self.display_surface)
 
-        # Player
+        # Player drawing and update
         all_obstacles = (
             self.border.sprites() + self.fixed.sprites() + self.breakables.sprites()
         )
         self.player_one.update(all_obstacles)
         self.player_one.draw(self.display_surface)
 
-        # Bomb
+        # Bomb drawing and update
         p = self.player_one.sprites()[0]
         if p.has_triggered_bomb:
-            b = Bomb((p.rect.x, p.rect.y))
-            if not pygame.sprite.spritecollideany(b, self.bomb_group):
-                self.bomb_group.add(b)
             p.has_triggered_bomb = False
+            if self.can_set_bomb(p.rect.x, p.rect.y):
+                b = Bomb((p.rect.x, p.rect.y))
+                self.bomb_group.add(b)
+                self.set_bomb_dispersion(b)
+
         self.bomb_group.draw(self.display_surface)
         self.bomb_group.update()
+        self.explode_breakables()
