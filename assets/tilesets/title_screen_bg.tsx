b648757010b783ba41d84a62fb4cf211429d66b2
Mel Tamplin <mei.chohee@gmail.com>
2022-10-05 13:44:30 -0400

    feat(assets): some title screen stuff
    Signed-off-by: Mel Tamplin <mei.chohee@gmail.com>

---
+++ assets/tilesets/title_screen_bg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="title_screen_bg" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="title_screen_bg.png" width="32" height="32"/>
+</tileset>
