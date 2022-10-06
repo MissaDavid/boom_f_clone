8a1dfaee7c34cda9c68070cc18574f75dd648e01
Mel Tamplin <mei.chohee@gmail.com>
2022-10-06 10:15:21 -0400

    feat(title_screen): display the game logo
    Signed-off-by: Mel Tamplin <mei.chohee@gmail.com>

--- title_screen.py
+++ title_screen.py
@@ -1,5 +1,6 @@
 import pygame as py
 
+from game_settings import ASSET_FOLDER, WIDTH, HEIGHT
 from tile import Tile
 from utils import create_tile_group_for_asset
 
@@ -15,6 +16,9 @@ class TitleScreen:
         self.background = create_tile_group_for_asset(
             layout.get("background"), "title_screen_bg", Tile
         )
+        self.logo = py.image.load(f"{ASSET_FOLDER}/boom_logo.png").convert_alpha()
+        self.logo_rect = self.logo.get_rect(center=(WIDTH / 2, HEIGHT / 3))
 
     def draw(self):
         self.background.draw(self.display_surface)
+        self.display_surface.blit(self.logo, self.logo_rect)
