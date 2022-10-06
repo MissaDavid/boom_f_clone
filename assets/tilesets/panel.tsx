b6f0411dcaab52358602b3d37cf7cbc8035128b2
Mel Tamplin <mei.chohee@gmail.com>
2022-10-06 10:04:03 -0400

    style(assets): make the map larger to accommodate for a side panel
    Signed-off-by: Mel Tamplin <mei.chohee@gmail.com>

---
+++ assets/tilesets/panel.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="panel" tilewidth="32" tileheight="32" tilecount="39" columns="3">
+ <image source="panel.png" width="96" height="416"/>
+</tileset>
